62a04cffea5c0a75271172122c4b304a64c9d1de
Tclack88 <tclack@aol.com>
2023-10-29 22:52:30 -0700

    add several javascript exercises

---
+++ js-practice/alert-quiz/temp.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", (event) =>{
+
+let answer = prompt("What is the \"official\" name of javascript?");
+
+disp = (answer == "ECMAScript") ? "Right!" : "You don't know? \"ECMAScript\"!";
+
+alert(disp);
+});
