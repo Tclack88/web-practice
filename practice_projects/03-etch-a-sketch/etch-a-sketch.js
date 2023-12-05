8df811037167366e977895c07838d26c48050058
Tclack88 <tclack@aol.com>
2023-12-04 21:44:39 -0800

    made the etch-a-sketch box square and it responds to mouse clicks (just not mouse moves yet)

--- practice_projects/03-etch-a-sketch/etch-a-sketch.js
+++ practice_projects/03-etch-a-sketch/etch-a-sketch.js
@@ -1,6 +1,29 @@
+let SIDE_LENGTH = 16;
+
 let container = document.querySelector("#container");
-for (let i=0; i<= 16*16; i++){
+
+// TO DEBUG: trying to set style here instead of hardcoding in stylesheet
+// let box = document.querySelector(".box");
+let box_padding = 10;
+let box_border = 1;
+// box.style.cssText=`padding: ${box_padding}px; border: ${box_border}px;`
+let box_side = `${(box_border + box_padding)*2*SIDE_LENGTH}px`;
+container.style.width = box_side;
+
+for (let i=0; i< SIDE_LENGTH**2; i++){
     let div_i = document.createElement("div");
     div_i.classList.add('box');
+    // div_i.addEventListener("pointerdown", (event) => {
+    //     let button = event.buttons;
+    //     console.log(button);
+    //     if (button == 1){
+    //         div_i.style.backgroundColor = 'black';
+    //     }
+    // });
+
+    div_i.addEventListener("pointermove" , (event) =>{
+        div_i.style.backgroundColor = 'black';
+    });
+
     container.appendChild(div_i);
 }
