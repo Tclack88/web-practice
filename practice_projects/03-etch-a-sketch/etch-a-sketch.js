81e770ba22a39ff7ff7bf5e630fdbec70e7054d3
Tclack88 <tclack@aol.com>
2023-12-06 10:52:43 -0800

    etch-a-sketch now works with pressing and dragging mosue. Added reset button. Allow for dynamic setting of padding, border and side length in javascript only to not rely on CSS

--- practice_projects/03-etch-a-sketch/etch-a-sketch.js
+++ practice_projects/03-etch-a-sketch/etch-a-sketch.js
@@ -1,29 +1,43 @@
-let SIDE_LENGTH = 16;
+let SIDE_LENGTH = 50;
 
 let container = document.querySelector("#container");
 
 // TO DEBUG: trying to set style here instead of hardcoding in stylesheet
+let box_padding = 3;
+let box_border = 0;
 // let box = document.querySelector(".box");
-let box_padding = 10;
-let box_border = 1;
+// console.log(box.style.cssText);
 // box.style.cssText=`padding: ${box_padding}px; border: ${box_border}px;`
 let box_side = `${(box_border + box_padding)*2*SIDE_LENGTH}px`;
 container.style.width = box_side;
 
+
 for (let i=0; i< SIDE_LENGTH**2; i++){
     let div_i = document.createElement("div");
     div_i.classList.add('box');
-    // div_i.addEventListener("pointerdown", (event) => {
-    //     let button = event.buttons;
-    //     console.log(button);
-    //     if (button == 1){
-    //         div_i.style.backgroundColor = 'black';
-    //     }
-    // });
+    div_i.style.cssText=`padding: ${box_padding}px; border: ${box_border}px black solid;`
 
-    div_i.addEventListener("pointermove" , (event) =>{
-        div_i.style.backgroundColor = 'black';
+    div_i.addEventListener("mousedown" , (ev) =>{
+        if (ev.buttons == 1){
+            div_i.style.backgroundColor = 'black';
+        }
     });
+
+    div_i.addEventListener("mouseover" , (ev) =>{
+        if (ev.buttons == 1){
+            div_i.style.backgroundColor = 'black';
+        }
+    });
+
 
     container.appendChild(div_i);
 }
+
+let resetButton = document.querySelector('#reset');
+resetButton.addEventListener('click', ()=> {
+    let boxes = Array.from(container.children);
+    boxes.forEach(element => {
+        element.style.backgroundColor = '#EEE';
+    });
+    // container.children.map.style.backgroundColor = '#EEE';
+});
